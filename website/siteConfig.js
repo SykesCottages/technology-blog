1d0c51b4f70d20e736329de7b22b98f7166243c3
Scott Dutton <exussum12@users.noreply.github.com>
2021-06-11 13:12:22 +0100

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -3,7 +3,7 @@ const siteConfig = {
   disableHeaderTitle: true,
   tagline: 'Technology Blog',
   url: 'https://sykes.dev',
-  baseUrl: '/',
+  baseUrl: '/technology-blog/',
   projectName: 'technology-blog',
   organizationName: 'sykescottages',
   headerLinks: [{
